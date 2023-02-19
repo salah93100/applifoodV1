39a488aa309461fbe38f3b07446bb37202e7b64f
Salah <salah.elbouhali@digityourdream.fr>
2023-02-19 01:31:10 +0100

    build top bar and create pages files

--- applifoodv1/src/App.js
+++ applifoodv1/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import UserRestaurant from './components/UserRestaurant';
+import Topbar from './components/Topbar';
+import Orders from './pages/Orders';
+
 function App() {
   return (
-    <div className="flex">
-      <div className='flex-4 bg-[#F8F9FB] border border-r-[#E5E9F2] h-screen text-[#42526E] w-64'>
+    <div className="flex w-full text-[#42526E] h-full">
+      
+      <div className='flex-4 bg-[#F8F9FB] border border-r-[#E5E9F2] h-screen  w-64 fixed'>
         <UserRestaurant/>
      <Sidebar/>
      </div>
-     <div className='flex-1'>
-    
+     <div className='flex-1 sticky ml-64 items-center '>
+        <Topbar/>
+        <Orders/>
      </div>
     </div>
   );
---
+++ applifoodv1/src/components/Topbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import {MdNotificationsNone,MdAccountCircle} from "react-icons/md";
+
+const Topbar=({tittle,numbernNotification})=> {
+  return (
+    <div className='h-[50px] flex border-y border-y-[#E5E9F2]  top-0 m-0 px-4'>
+        <div className='flex  justify-between mx-4 items-center h-full w-full'>
+            <div className='flex items-center'>
+            
+            <p className='font-semibold text-[16px] '>Orders</p>
+            </div>
+
+            <div className='flex items-center justify-between min-w-[94px]'>
+              <div className='flex relative '>
+            <MdNotificationsNone size={28}/>
+            <span className="flex items-center justify-center  absolute bg-red-600 rounded-full h-[14px] w-[14px] -top-[4px] -right-[2px] text-white text-[10px] font-semibold">2</span>
+            </div>
+
+            <div className='flex border-l border-l-[#888888] pl-[18px]'>
+              <MdAccountCircle size={30}/>
+            </div>
+            
+           
+            </div>
+        </div>
+      
+    </div>
+   
+  )
+}
+export default Topbar 
