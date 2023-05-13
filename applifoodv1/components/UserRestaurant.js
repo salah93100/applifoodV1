eba14d242b13655032d557f47e9be8e8782e871a
salah93100 <salah.elbouhali@digityourdream.fr>
2023-05-13 21:54:18 +0200

    Ligth kitch

--- applifoodv1/components/UserRestaurant.js
+++ applifoodv1/components/UserRestaurant.js
@@ -12,7 +12,7 @@ export default function UserRestaurant({src,alt,nameRestaurant}) {
       className=''/>
       </div>
     <div className='text-center'>
-        <p className='font-semibold uppercase'>ID: Light Kitchen</p>
+        <p className='font-semibold uppercase'>ID: Light Kitch</p>
     </div>
     </div>
   )
